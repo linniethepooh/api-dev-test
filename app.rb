9b100fe48bc99c8da1c06b78735e6c5f2b46f9d2
Lindelwe Ncube <linniencube@gmail.com>
2018-10-09 21:51:17 +0100

    implemented command line arg

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
   require './lib/purchase'
   require './lib/user'
 
-  action = gets.chomp
-
-  if action == 'most_sold'
-    puts Purchase.most_sold # most_sold
-  elsif action == 'most_loyal'
-    puts User.most_loyal # most_loyal
-  else
-    array = action.split(' ')
-    email = array[1]
-    puts User.total_spend(email) #total_spent
+  ARGV.each do|action|
+    if action == 'most_sold'
+      puts Purchase.most_sold # most_sold
+    elsif action == 'most_loyal'
+      puts User.most_loyal # most_loyal
+    else
+      email = ARGV[1]
+      puts User.total_spend(email) #total_spent
+    end
   end
+
 
