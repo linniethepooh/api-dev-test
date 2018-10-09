a01628115094338b80030da247b6ac2648af4870
Lindelwe Ncube <linniencube@gmail.com>
2018-10-10 00:18:47 +0100

    update

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
-  require './lib/purchase'
-  require './lib/user'
+require './lib/purchase'
+require './lib/user'
 
-  ARGV.each do|action|
-    if action == 'most_sold'
-      puts Purchase.most_sold # most_sold
-    elsif action == 'most_loyal'
-      puts User.most_loyal # most_loyal
-    else
-      email = ARGV[1]
-      puts User.total_spend(email) #total_spent
-    end
+ARGV.each do |action|
+  if action == 'most_sold'
+    puts Purchase.most_sold # most_sold
+  elsif action == 'most_loyal'
+    puts User.most_loyal # most_loyal
+  else
+    email = ARGV[1]
+    puts User.total_spend(email) #total_spent
   end
+end
 
 
