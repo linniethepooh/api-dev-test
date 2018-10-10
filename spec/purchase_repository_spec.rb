605b57d91ca487c610a5f8c5f70b39215b4c3463
Lindelwe Ncube <linniencube@gmail.com>
2018-10-10 14:45:14 +0100

    update

--- spec/purchase_repository_spec.rb
+++ spec/purchase_repository_spec.rb
@@ -1,8 +1,13 @@
-require_relative "../lib/repositories/meal_repository"
-
-describe "PurchaseRepository", :purchase  do
-
-  let(:purchase_json) { "https://driftrock-dev-test.herokuapp.com/purchases" }
+require_relative "../lib/repositories/purchase_repository"
 
 
-end
+# describe "PurchaseRepository", :purchase  do
+
+#   let(:purchase_json) { "https://driftrock-dev-test.herokuapp.com/purchases" }
+
+#   describe "#all" do
+#     repo = PurchaseRepository.new(purchase_json)
+#     expect(repo.all).to be_a(Array)
+#   end
+
+# end
